b5d8900692df85b003f283220190763daeb4c2b3
vincentexelcio_ <117747910+vincnx@users.noreply.github.com>
2024-06-08 20:36:18 +0700

    feat(berita): menambahkan halaman indeks berita (#6)
    Co-authored-by: vincent exelcio <vincentexelcio@vincent.local>

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
      */
     public function index()
     {   
-        $posts = Post::orderBy('waktu_upload', 'desc')->paginate(10);
+        $posts = Post::orderBy('waktu_upload', 'desc')->Paginate(7);
         return view('berita', compact('posts'));
     }
 
