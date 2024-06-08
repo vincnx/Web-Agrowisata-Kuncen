d9788172535f8a1c3b5ff415bfc59212b885fc0c
vincentexelcio_ <117747910+vincnx@users.noreply.github.com>
2024-06-08 20:57:58 +0700

    fix(card): memperbaiki tampilan (#7)
    * fix(card): memperbaiki gambar pada card
    
    * fix(card-pengumuman): memperbaiki gambar pada card pengumuman
    
    ---------
    
    Co-authored-by: vincent exelcio <vincentexelcio@vincent.local>

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,12 +1,14 @@
 @props(['title', 'src', 'desc'])
-<div {{ $attributes->merge(['class' => "bg-white border border-gray-200 p-4 rounded-lg shadow shadow-xl dark:bg-gray-800 dark:border-gray-700 lg:col-span-3"]) }}>
-    <div class="bg-center bg-cover rounded-lg" style="background-image: url('{{ $src }}')">
-          <img class="rounded-lg aspect-square invisible" src="{{ $src }}" alt="" />
-    </div>
+<div
+  {{ $attributes->merge(['class' => 'bg-white border border-gray-200 p-4 rounded-lg shadow shadow-xl dark:bg-gray-800 dark:border-gray-700 lg:col-span-3']) }}>
+  {{-- image --}}
+  <img class="aspect-square w-full rounded-lg object-cover" src="{{ $src }}" alt="" />
+  
+  {{-- description --}}
   <div class="text-center">
-      <a href="#">
-          <h5 class="my-5 text-xl font-semibold text-green-page dark:text-white">{{ $title }}</h5>
-      </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $desc }}</p>
+    <a href="#">
+      <h5 class="text-green-page my-5 text-xl font-semibold dark:text-white">{{ $title }}</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $desc }}</p>
   </div>
 </div>
