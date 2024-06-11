922142a7fe7f53960bba19870ba3b59a2f77c808
vincentexelcio_ <117747910+vincnx@users.noreply.github.com>
2024-06-11 16:26:35 +0700

    feat(camp-package): menambahkan model & migration (#11)
    Co-authored-by: vincent exelcio <vincentexelcio@vincent.local>

---
+++ app/Http/Controllers/CampPackageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCampPackageRequest;
+use App\Http\Requests\UpdateCampPackageRequest;
+use App\Models\CampPackage;
+
+class CampPackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCampPackageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CampPackage $campPackage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CampPackage $campPackage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCampPackageRequest $request, CampPackage $campPackage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CampPackage $campPackage)
+    {
+        //
+    }
+}
