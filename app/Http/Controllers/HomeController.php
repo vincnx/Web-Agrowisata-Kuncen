5a6ba7e7b735b759371539917e04115cc411ac23
vincent exelcio <vincentexelcio@vincent.local>
2024-06-11 16:04:22 +0700

    feat(index): mengubah harga paket camping diambil dari database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CampPackage;
 use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -11,6 +12,7 @@ class HomeController extends Controller
     public function index(){
         Carbon::setLocale('id');
         $posts = Post::orderBy('waktu_upload', 'desc')->paginate(3);
-        return view('index', compact('posts'));
+        $camp_packages = CampPackage::all();
+        return view('index', compact('posts', 'camp_packages'));
     }
 }
