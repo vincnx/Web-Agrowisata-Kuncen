ceae45f69b1ea096af7da23bd9cd8a1d66fc8a3e
vincentexelcio_ <117747910+vincnx@users.noreply.github.com>
2024-06-12 11:24:04 +0700

    feat(berita-index): update desain (#14)
    * feat(berita-index): mengubah desain menjadi responsif
    
    * fix(berita-index): update layout halaman
    
    ---------
    
    Co-authored-by: vincent exelcio <vincentexelcio@vincent.local>

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -1,38 +1,44 @@
 @extends('layouts.template')
 
 @section('content')
-  <div class="container mx-auto px-10">
-    <h1 class="text-dark-blue-page my-10 pt-10 text-3xl font-semibold">Berita Agrowisata Kuncen</h1>
+  <div class="mx-auto px-10 sm:container">
+    <h1 class="text-dark-blue-page my-10 text-center text-2xl font-semibold lg:pt-10 lg:text-left lg:text-3xl">Berita
+      Agrowisata Kuncen</h1>
     @if ($posts->count())
       {{-- berita utama --}}
-      <div class="mb-20">
+      <div class="mb-10 lg:mb-20">
         {{-- <h2 class="text-2xl">Berita Terbaru</h2> --}}
-        <div class="grid h-[450px] grid-cols-12 gap-x-7">
-          <img src="{{ Storage::url($post->gambar) }}" alt="" class="col-span-6 h-full w-full object-cover">
-          <div class="col-span-6 flex items-center">
-            <div class="text-justify">
+        <div class="grid grid-cols-2 gap-x-7 md:h-[300px] lg:h-[500px]">
+          <img src="{{ Storage::url($post->gambar) }}" alt=""
+            class="col-span-2 h-full max-h-[500px] w-full rounded-md object-cover shadow-lg md:col-span-1">
+          <div class="col-span-2 flex items-center md:col-span-1">
+            <div class="mt-5 text-justify md:mt-0">
               <div class="mb-5">
                 <h3 class="text-dark-blue-page mb-5 text-2xl font-semibold">{{ $post->judul }}</h3>
-                <p class="text-dark-blue-page leading-5">{!! Str::words(strip_tags($post->isi), 120, '...') !!}</p>
+                <p class="text-dark-blue-page leading-5">{!! Str::words(strip_tags($post->isi), 20, '...') !!}</p>
+                <p class="text-dark-blue-page hidden leading-5 lg:block">{!! Str::words(strip_tags($post->isi), 80, '...') !!}</p>
               </div>
-              <a href="berita/{{ $post->slug }}"
-                class="'inline-flex bg-main-page duration-150' items-center rounded-md border border-transparent px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300">Baca
-                Selengkapnya</a>
+              <a href="berita/{{ $post->slug }}">
+                <x-primary-button>Baca Selengkapnya</x-primary-button>
+              </a>
             </div>
           </div>
         </div>
       </div>
+  </div>
 
-      {{-- berita lainnya --}}
-      <article class="mb-5 grid grid-cols-12 gap-6">
-        @foreach ($posts as $post)
-          <x-card-pengumuman src="{{ Storage::url($post->gambar) }}" judul="{{ $post->judul }}"
-            isi="{{ Str::words(strip_tags($post->isi), 20, '...') }}"
-            waktu_upload="{{ Carbon\Carbon::parse($post->waktu_upload)->translatedFormat('l, d F Y') }}"
-            slug="{{ $post->slug }}" class="col-span-4" />
-        @endforeach
-      </article>
-      {{ $posts->links() }}
+  {{-- berita lainnya --}}
+  <div class="container mx-auto px-10">
+    <h2 class="text-dark-blue-page mb-5 text-2xl font-semibold">Berita Lainnya</h2>
+    <article class=" mb-5 grid grid-cols-12 gap-7">
+      @foreach ($posts as $post)
+        <x-card-pengumuman src="{{ Storage::url($post->gambar) }}" judul="{{ $post->judul }}"
+          isi="{{ Str::words(strip_tags($post->isi), 20, '...') }}"
+          waktu_upload="{{ Carbon\Carbon::parse($post->waktu_upload)->translatedFormat('l, d F Y') }}"
+          slug="{{ $post->slug }}" class="col-span-12 md:col-span-4" />
+      @endforeach
+    </article>
+    {{ $posts->links() }}
   </div>
   @endif
 
