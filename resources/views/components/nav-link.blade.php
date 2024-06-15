0dfab4eebb68e720155ac4459809f69f2de0cd99
vincent exelcio <vincentexelcio@vincent.local>
2024-06-15 21:39:14 +0700

    fix(navbar): merubah ke format terbaru

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-main-page dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-dark-blue-page hover:text-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+            : 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-dark-blue-page hover:text-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out hover:text-main-page transition-colors';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
