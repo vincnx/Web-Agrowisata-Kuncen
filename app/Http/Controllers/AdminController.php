8ae03aab56d8d8b44acf06622bb68450937643c7
vincentexelcio_ <117747910+vincnx@users.noreply.github.com>
2024-06-24 20:32:32 +0700

    Fix/admin index (#29)
    * fix(admin): fix navbar & tabel
    
    * fix(admin-index): menambahkan fungsi upload gambar untuk harga
    
    ---------
    
    Co-authored-by: vincent exelcio <vincentexelcio@vincent.local>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,17 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\CampPackage;
-use App\Models\Message;
 use App\Models\Post;
+use App\Models\Message;
+use App\Models\PriceImage;
+use App\Models\CampPackage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
     public function index(){
         $posts = Post::orderBy('waktu_upload', 'desc')->paginate(5);
-        $packages = CampPackage::paginate(4);
+        $packages = CampPackage::paginate(5);
         $messages = Message::paginate(4);
-        return view('admin.index', compact('posts', 'packages', 'messages'));
+        $price_images = PriceImage::first();
+        return view('admin.index', compact('posts', 'packages', 'messages', 'price_images'));
+    }
+
+    public function edit(Request $request, PriceImage $price_image){
+
+        $validated = $request->validate([
+            'makanan'=>'image',
+            'peta'=>'image',
+            'peralatan'=>'image'
+        ]);
+        if ($request->type == 'makanan'){
+            Storage::delete($price_image->makanan);
+            $makanan = $request->file('img_file')->store('public/price-images/makanan');
+            $validated['makanan'] = $makanan;
+        }
+        elseif ($request->type == 'peta'){
+            Storage::delete($price_image->peta);
+            $peta = $request->file('img_file')->store('public/price-images/peta');
+            $validated['peta'] = $peta;
+        }
+        elseif ($request->type == 'peralatan'){
+            Storage::delete($price_image->peralatan);
+            $peralatan = $request->file('img_file')->store('public/price-images/peralatan');
+            $validated['peralatan'] = $peralatan;
+        }
+        $price_image->update($validated);
+
+        return to_route('admin.index');
     }
 }
---
+++ app/Http/Controllers/PriceImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePriceImageRequest;
+use App\Http\Requests\UpdatePriceImageRequest;
+use App\Models\PriceImage;
+
+class PriceImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePriceImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PriceImage $priceImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PriceImage $priceImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePriceImageRequest $request, PriceImage $priceImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PriceImage $priceImage)
+    {
+        //
+    }
+}
